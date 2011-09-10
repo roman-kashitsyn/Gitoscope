81a71c4173b145f122bdd2fbd2c8e99df6ac4307
Roman <roman.kashitsyn@gmail.com>
2011-09-11 00:05:26 +0400

    Refactor controller's code to take make them less verbose.

--- src/main/java/gitoscope/controller/ProjectController.java
+++ src/main/java/gitoscope/controller/ProjectController.java
@@ -1,7 +1,9 @@
 package gitoscope.controller;
 
 import java.util.*;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,28 +13,28 @@ import gitoscope.service.ProjectService;
 @Controller
 public class ProjectController {
 
-	@RequestMapping(value="/project/list", method=RequestMethod.GET)
-	public ModelAndView list() {
-		List<Project> projectList = projectService.listProjects();
-		return new ModelAndView("project.list", "projectList", projectList);
-	}
+    @RequestMapping(value = "/project/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        List<Project> projectList = projectService.listProjects();
+        model.addAttribute(projectList);
+        return "project.list";
+    }
 
-	@RequestMapping(value="/project/{projectName}.git", method=RequestMethod.GET)
-	public ModelAndView show(@PathVariable String projectName) {
+    @RequestMapping(value = "/project/{projectName}.git", method = RequestMethod.GET)
+    public String show(@PathVariable String projectName, Model model) {
 
-		Project project = projectService.findProjectByName(projectName);
-		List<Commit> commits = projectService.listCommits(project, null);
+        Project project = projectService.findProjectByName(projectName);
+        List<Commit> commits = projectService.listCommits(project, null);
 
-		ModelAndView modelAndView = new ModelAndView("project.show");
-		modelAndView.addObject("project", project);
-		modelAndView.addObject("title", project.getName() + ".git");
-		modelAndView.addObject("commits", commits);
-		return modelAndView;
-	}
+        model.addAttribute(project);
+        model.addAttribute("title", project.getName() + ".git");
+        model.addAttribute("commits", commits);
+        return "project.show";
+    }
 
-	public void setProjectService(ProjectService projectService) {
-		this.projectService = projectService;
-	}
+    public void setProjectService(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
-	private ProjectService projectService;
+    private ProjectService projectService;
 }
